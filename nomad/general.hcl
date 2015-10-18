194c3c576ce4501e36c6a5b4a71ae8d4ecdb9abf
Erik Veld <erik.veld@gmail.com>
2015-10-18 15:27:36 +0200

    Added nomad config

---
+++ nomad/general.hcl
@@ -0,0 +1,19 @@
+# Location
+region = ""
+datacenter = ""
+
+# Node
+name = ""
+bind_addr = ""
+data_dir = "/var/local/nomad"
+
+# Logging
+enable_debug = true
+enable_syslog = true
+syslog_facility = "LOCAL0"
+log_level = "INFO"
+
+# Metrics
+telemetry = {
+  statsd_address = ""
+}
--- packer/scripts/packages.sh
+++ packer/scripts/packages.sh
@@ -2,16 +2,30 @@
 
 # Install tools
 apt-get update
-apt-get install -y curl git unzip
+apt-get install -y curl unzip build-essential
 
 # Install Docker
 curl -sSL https://get.docker.com/ | sh
 
 # Install Nomad
+cd /tmp/
 wget -O nomad.zip https://dl.bintray.com/mitchellh/nomad/nomad_0.1.2_linux_amd64.zip
 unzip nomad.zip
-rm nomad.zip
+
 mv nomad /usr/bin/nomad
 chmod +x /usr/bin/nomad
+
 mkdir /etc/nomad
-chmod a+w /etc/nomad
+chmod 755 /etc/nomad
+
+mkdir /var/local/nomad
+chmod 777 /var/local/nomad
+
+# Install Collectd
+cd /tmp/
+wget https://github.com/collectd/collectd/archive/collectd-5.5.0.zip
+unzip collectd-5.5.0.zip
+cd collectd-5.5.0
+
+./configure
+make all install
