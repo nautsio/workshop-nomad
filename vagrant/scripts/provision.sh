f2a399cb012d9b80e0abacc5bbc6eb4bb4a97a35
Bastiaan Bakker <bbakker@xebia.com>
2015-11-12 22:36:44 +0100

    fix ip addresses in /etc/hosts. from Vagrantfile supply base address to scripts/provision.sh

--- vagrant/scripts/provision.sh
+++ vagrant/scripts/provision.sh
@@ -2,35 +2,39 @@
 MACHINE_PREFIX=$1
 MACHINE_ID=$2
 NUM_INSTANCES=$3
+BASE_ADDRESS=$4
 
-function writeNomadNodeConfig() {
-  local NAME=$(printf "$MACHINE_PREFIX-%02d" $MACHINE_ID)
-  local BINDADDR="172.17.8.$((100+$MACHINE_ID))"
-  printf 'name="%s"\nbind_addr="%s"' $NAME $BINDADDR > /etc/nomad.d/node.hcl
+BASE_OCTET1_3=$(echo $BASE_ADDRESS | cut -d. -f1-3)
+BASE_OCTET4=$(echo $BASE_ADDRESS | cut -d. -f4)
+
+function nodeNumberToAddress {
+  echo "${BASE_OCTET1_3}.$((${BASE_OCTET4} + $1))"
 }
 
-function generateNodeHostsLine() {
-  local HOST=$(printf "$MACHINE_PREFIX-%02d" $MACHINE_ID)
-  local IP="172.17.8.$((100+$MACHINE_ID))"
-  echo "$IP   $HOST"
+NAME=$(printf "%s-%02d" $MACHINE_PREFIX $MACHINE_ID)
+BIND_ADDR="$(nodeNumberToAddress $MACHINE_ID)"
+
+function writeNomadNodeConfig {
+  printf 'name="%s"\nbind_addr="%s"\n' $NAME $BIND_ADDR > /etc/nomad.d/node.hcl
 }
 
-function writeConsulNodeConfig() {
-  local BINDADDR="172.17.8.$((100+$MACHINE_ID))"
-  if [ $MACHINE_ID == 1 ]; then
-    printf '{"bootstrap": true, "bind_addr": "%s", "client_addr": "%s"}' $BINDADDR $BINDADDR | jq . > /etc/consul.d/node.json
-  else
-    printf '{"start_join": ["172.17.8.101"], "bind_addr": "%s", "client_addr": "%s"}' $BINDADDR $BINDADDR | jq . > /etc/consul.d/node.json
-  fi
+function writeConsulNodeConfig {
+  (
+    if [ $MACHINE_ID == 1 ]; then
+      printf '{"bootstrap": true, "bind_addr": "%s", "client_addr": "%s"}' $BIND_ADDR $BIND_ADDR
+    else
+      printf '{"start_join": ["%s"], "bind_addr": "%s", "client_addr": "%s"}' $(nodeNumberToAddress 1) $BIND_ADDR $BIND_ADDR
+    fi
+  ) | jq . > /tmp/etc/consul.d/node.json
 }
 
-function writeHostsFile() {
-  local LOCALHOST="127.0.0.1    localhost"
-  echo $LOCALHOST > /etc/hosts
-  for ((i=1; i<=$NUM_INSTANCES; i++))
-  do
-    echo $(generateNodeHostsLine $i) >> /etc/hosts
-  done
+function writeHostsFile {
+  (
+    printf "%-15s %s\n" 127.0.0.1 localhost
+    for i in $(seq $NUM_INSTANCES); do
+      printf "%-15s %s-%02d\n" $(nodeNumberToAddress $i) $MACHINE_PREFIX $i
+    done
+  ) > /etc/hosts
 }
 
 writeHostsFile
